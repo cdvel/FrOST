4fd4ce95bf935b67e2dc7023e254412b299e6019
cdario <cd.velandia@gmail.com>
2013-09-29 21:33:17 +0800

    removed plugin files and tested

--- FrOST.Simulation/QPPrediction.cpp
+++ FrOST.Simulation/QPPrediction.cpp
@@ -1,5 +1,30 @@
-#include "stdafx.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <vector>
+#include <math.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <string>
+#include <time.h> 
+#include <windows.h>
+#include <process.h>
+#include<random>
+
+extern "C" {
+#include "programmer.h"
+}
+
+#include "COP97A.h"
+#include "QPStates.h"
+#include "QPParameters.h"
+#include "QPPrediction.h"
+#include "QPController.h"
 using namespace std;
 
 namespace QPSim{
